4b6e40ad4c1391a7fc94a2470d4542ce629c0f68
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-04-21 11:53:27 +0000

    Beginnings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
+  layout("application.html.erb")
+  def homepage
+    #Pull the page homepage
+    render({ :template => "form_templates/homepage".html_safe })
+  end
+
+  def pay_me
+    #Pull the page pay_me
+    render({ :template => "form_templates/pay_me".html_safe })
+  end
+
+  def rando
+    #Pull the page rando
+    render({ :template => "form_templates/rando".html_safe })
+  end
+
+  def square_root
+    #Pull the page square_root
+    render({ :template => "form_templates/square_root".html_safe })
+  end
+
+  def square
+    #Pull the page square
+    render({ :template => "form_templates/square".html_safe })
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-
+  self.get("/square/new", { :controller => "application", :action => "square"})
+  self.get("/square_root/new", { :controller => "application", :action => "square_root"})
+  self.get("/payment/new", { :controller => "application", :action => "pay_me"})
+  self.get("/random/new", { :controller => "application", :action => "rando"})
+  self.get("/", { :controller => "application", :action => "homepage"})
 end
